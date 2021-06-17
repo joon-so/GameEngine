48cfd20bf029a2f8d679bc1b3e3943870e0d587e
Lee Mi Reuk <sslejds@naver.com>
2021-06-17 22:07:41 +0900

    Enemy Hp Bar

---
+++ Asus/Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform cam;
+    private GameObject mainCamera;
+
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        cam = mainCamera.transform;
+    }
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
